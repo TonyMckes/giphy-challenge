0572065d0ec00d1fe9ec9be8eaf4dc6aac7cacf0
TonyMckes <tonymckes@gmail.com>
2022-04-17 14:30:25 -0400

    Added max height to "vertical" images

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,12 +1,12 @@
 function Image({ image, title, small }) {
-  const { url, webp } = image || {};
+  const { url, webp, height, width } = image || {};
 
   const onLoadHandler = (e) => {
     e.target.classList.replace("opacity-0", "opacity-100");
   };
 
   return (
-    <picture className="w-[31rem]">
+    <picture className={`${height > width ? "h-[31rem]" : "w-[31rem]"}`}>
       <source srcSet={webp} type="image/webp" />
       <img
         className={`object-cover w-full transition-opacity duration-300 ease-in opacity-0 ${
